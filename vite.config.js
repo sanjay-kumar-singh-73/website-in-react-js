df810041a6a73ebf53857a69e3d3b6ba344b7d76
sanjay-kumar-singh-73 <sanjaychitavsram9759@gamil.com>
2024-05-25 01:00:25 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/react-js/",
+  base:"/projectreactjs/",
   plugins: [react()],
 
 })
